af7bc677422190cfdfa8887303fe70ef2b9af442
BenWhann <whann004@umn.edu>
2024-01-17 11:13:27 -0600

    created pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/flower' element={<ShopCategory category="flower"/>}/>
+        <Route path='/hash' element={<ShopCategory category="hash"/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/Pages/ShopCategory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopCategpry = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default ShopCategpry;
