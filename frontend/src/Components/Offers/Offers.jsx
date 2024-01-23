c9ffe7663b527cc01ae0f96feb13eb0edfc8e2f1
BenWhann <whann004@umn.edu>
2024-01-23 13:01:22 -0600

    offers component and styling

--- frontend/src/Components/Offers/Offers.jsx
+++ frontend/src/Components/Offers/Offers.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './Offers.css'
+import afghan_kush_live from '../Assets/afghan_kush_live.JPG'
 
 const Offers = () => {
   return (
-    <div>Offers</div>
+    <div className='offers'>
+        <div className="offers-left">
+            <h1>Exclusive</h1>
+            <h1>Offers For You</h1>
+            <p>ONLY ON BEST SELLING PRODUCTS</p>
+            <button>Check Now</button>
+        </div>
+
+        <div className="offers-right">
+            <img className='offersImg' src={afghan_kush_live} alt="" />
+        </div>
+    </div>
   )
 }
 
