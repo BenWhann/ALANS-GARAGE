9569d7ead062da85cb5bdc7a8f13d4b60f369072
BenWhann <whann004@umn.edu>
2024-01-25 16:38:57 -0600

    sign up html

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 const LoginSignup = () => {
     return (
-        <div>
-
+        <div className='loginsignup'>
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder='Username' />
+                    <input type="email" placeholder='Email Address'/>
+                    <input type="password" placeholder='Password' />
+                </div>
+                <button>Sign Up</button>
+                <p className="loginsignup-login">Already have an account with us? <span>Login Here</span></p>
+                <div className="loginsignup-agreement">
+                    <input type="checkbox" name='' id='' />
+                    <p>By choosing to continue, I agree to terms of use and privacy policy.</p>
+                </div>
+            </div>
         </div>
     )
 };
