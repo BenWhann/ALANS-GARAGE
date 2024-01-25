1d6d45f7e8e41657e5334d5efc2807e5c220f982
BenWhann <whann004@umn.edu>
2024-01-25 17:10:46 -0600

    starting individual product pages, but i must go gym now

---
+++ frontend/src/Components/Breadcrumbs/Breadcrumb.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Breadcrumb = () => {
+  return (
+    <div className='breadcrumb'>Breadcrumb</div>
+  )
+}
+
+export default Breadcrumb
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react';
 import { ShopContext } from '../Context/ShopContext.jsx'
+import { useParams } from 'react-router-dom'
 
 const Product = () => {
     const {products} = useContext(ShopContext);
+    const {productId} = useParams();
+    const product = products.find((event) => {event.id === Number(productId)});
     return (
         <div>
-
+            
         </div>
     )
 };
