4d7b7e0a44f6ca5ac4257af02e0d8484967d0272
BenWhann <whann004@umn.edu>
2024-01-25 17:04:15 -0600

    log in sign up css

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext.jsx'
 
 const Product = () => {
+    const {products} = useContext(ShopContext);
     return (
         <div>
 
